b8ff4d7d0cfe9a18c8349019f677d0028cf04bc5
juhongseon <hsjooi@hanmail.net>
2020-07-24 19:27:12 +0900

    Header...

--- src/components/commons/Header.js
+++ src/components/commons/Header.js
@@ -13,33 +13,16 @@ export default function Header() {
     const keyword = home.keyword
 
     return (
-        <nav className="navbar navbar-default">
+        <nav className="navbar navbar-default navbar-fixed-top">
             <div className="container-fluid">
                 <div className="navbar-header">
                     <NavLink exact to={'/Home'} className="navbar-brand">Eco</NavLink>
                 </div>
-                <ul className="nav navbar-nav">
-                    <li>
-                        <select onChange={(e)=>{dispatch(setHomeFilter1(e.target.value))}} value={filter1} style={{"height":"34px","border":"1px solid rgb(204,204,204)","borderRadius":"5px"}}>
-                            <option value="title"> 제목</option>
-                            <option value="tag"> 태그</option>
-                            <option value="author"> 작가</option>
-                        </select>
-                        <form action="#" style={{"display":"inline-block","width":"200px","position":"relative","top":"12px"}}>
-                            <div className="input-group">
-                                <input onFocus={()=>{dispatch(showSuggest())}} value={keyword} onChange={(e)=>{dispatch(setKeyword(e.target.value))}} type="text" className="form-control" placeholder="검색어를 입력하세요."/>
-                                <div className="input-group-btn">
-                                <NavLink to={'/Home'}>
-                                    <button className="btn btn-default" type="button">
-                                        <i className="glyphicon glyphicon-search"></i>
-                                    </button>
-                                </NavLink>
-                                </div>
-                            </div>
-                        </form>
-                    </li>
+                <ul id={"nav-items"} className="nav navbar-nav">
+                    <li><NavLink to={'/Popular'}>Popular</NavLink></li>
+                    <li><NavLink to={'/Category'}>Category</NavLink></li>
+                    <li><NavLink to={'/My'}>Search</NavLink></li>
                     <li><NavLink to={'/My'}>My</NavLink></li>
-                    <li onClick={()=>{dispatch(showAdditional())}} style={{"cursor":"pointer"}}>Adtn</li>
                 </ul>
             </div>
         </nav>
