e60adce63c843bfe540444680668771dc7338f00
juhongseon <hsjooi@hanmail.net>
2020-10-19 14:03:35 +0900

    add server/login

--- eco-server.js
+++ eco-server.js
@@ -3,9 +3,10 @@ const app = express()
 app.use(express.static('emo'))
 
 const mc = require("mongodb").MongoClient
-const ObjectID = require("mongodb").ObjectID
 
 const mUrl = 'mongodb://localhost:27017'
+
+const crypto = require('crypto')
 
 const shuffleArray = (origin)=>{
     let res = []
@@ -84,5 +85,52 @@ app.get('/search_by_category',(req,res)=>{
                 res.json(shuffleArray(docs))
                 client.close()
             })
+    })
+})
+
+app.get('/login',(req,res)=>{
+    var id = req.query.id
+    var pwd = req.query.pwd
+    if(id==undefined|pwd==undefined) {
+        res.json('error')
+        return
+    }
+
+    mc.connect(mUrl,(err,client)=>{
+        var db = client.db('eco')
+
+        db.collection('user').countDocuments({id:id},(err,idCount)=>{
+            if(idCount==1) {
+                db.collection('user').findOne({id:id},{salt:1,key:1},(err,dbData)=>{
+                    crypto.pbkdf2(pwd,dbData.salt,184265,64,'sha512',(err,key)=>{
+                        if(key.toString('base64')==dbData.key) {
+                            var auth = crypto.createHash('sha256').update(Date.now()+id).digest('base64')
+
+                            db.collection('user').updateOne({id:id},{$set:{auth:auth}},(err,res)=>{client.close})
+                            res.json(auth)
+                        }
+                        else {
+                            res.json('nopwd')
+                            client.close()
+                        }
+                    })
+                })
+            } else {
+                res.json('noid')
+                client.close()
+            }
+        })
+    })
+})
+
+app.get('/crypto',(req,res)=>{
+    var string = req.query.string
+
+    crypto.randomBytes(64,(err,buf)=>{
+        var salt = buf.toString('base64')
+
+        crypto.pbkdf2(string,salt,184265,64,'sha512',(err,key)=>{
+            res.json({salt:salt,key:key.toString('base64')})
+        })
     })
 })
